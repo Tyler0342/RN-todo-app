7fae63f85e3c7eeaae9f2f9676828a6a18a8ced5
tyler <tymaggio03@gmail.com>
2022-05-30 16:41:27 -0600

    Added button functionality

--- App.js
+++ App.js
@@ -1,20 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react'; 
+import { StyleSheet, Text, View, TextInput, Button} from 'react-native';
 
 export default function App() {
+  const [enteredListText, setEnteredListText] = useState('');
+  const [list, setList] = useState([]);
+
+  function textInputHandler(enteredText) {
+    setEnteredListText(enteredText);
+  };
+
+  function addInputHandler() {
+    setList(currentList => [...currentList, enteredListText ])
+  }; 
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+        <View style={styles.inputContainer}>
+          <TextInput style={styles.textInput} 
+          placeholder='Add a task'
+          onChangeText={textInputHandler} 
+          />
+          <Button title='Add' 
+          color='steelblue'  
+          onPress={addInputHandler}
+          />
+        </View> 
+       <View style={styles.listContainer}>
+         {list.map((item) => <Text key={list}>{list}</Text> )}
+       </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  appContainer: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 60,
+    paddingHorizontal: 20
   },
+  inputContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 10,
+    borderBottomWidth: 2,
+    borderBottomColor: 'powderblue'
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: 'powderblue',
+    width: '80%',
+    marginRight: 10,
+    padding: 4
+  },
+  listContainer: {
+    flex: 6
+  }
 });
